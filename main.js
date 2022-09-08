26c6c6931022e13bec81da2ef73dba9fdec86b25
Lev Vysotsky <vysotskylev@users.noreply.github.com>
2022-09-09 01:57:56 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("The extension is up and running")
+
+var s = document.createElement('script');
+s.src = chrome.runtime.getURL('modified_index_old.js');
+// s.src = chrome.runtime.getURL('index_old.js');
+s.onload = function() {
+    this.remove();
+};
+(document.head || document.documentElement).appendChild(s);
