be72edec441267c91699190f3c279d4bb94c18ae
Lev Vysotsky <vysotskylev@users.noreply.github.com>
2022-09-09 02:15:51 +0200

    File for current election

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 console.log("The extension is up and running")
 
 var s = document.createElement('script');
-s.src = chrome.runtime.getURL('modified_index_old.js');
-// s.src = chrome.runtime.getURL('index_old.js');
+// s.src = chrome.runtime.getURL('modified_index_old.js');
+s.src = chrome.runtime.getURL('modified_index.js');
 s.onload = function() {
     this.remove();
 };
